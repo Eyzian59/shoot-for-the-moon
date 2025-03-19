2dc4342ba96b3ee512106d7e77699ce4d55e2e2a
Eyzian59 <trunguyen5927@gmail.com>
2025-03-18 23:39:02 -0500

    Camera and Movement
    Added camera tracking and player movement

--- Spring Break/Assets/Scripts/spaceshipMovement.cs
+++ Spring Break/Assets/Scripts/spaceshipMovement.cs
@@ -4,19 +4,47 @@ using UnityEngine;
 
 public class spaceshipMovement : MonoBehaviour
 {
-    public Rigidbody2D rb;
-
-    // Variables for spaceship - thrustPower, current and max fuel, the speed of the rotation, altitude, reduction of the altitude, and heatRate
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float thrustSpeed = 5f;
+    public float tiltSpeed = 50f;
+    private bool isThrusting = false;
+    private float currentTilt = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        handleThrust();
+        if (isThrusting)
+        {
+            handleTilt();
+        }
+    }
+
+    void handleThrust()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isThrusting)
+        {
+            isThrusting = true;
+        }
+        if (isThrusting)
+        {
+            transform.Translate(Vector3.up * thrustSpeed * Time.deltaTime);
+        }
+    }
+
+    void handleTilt()
+    {
+        float tiltInput = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            tiltInput = 1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            tiltInput = -1f;
+        }
+        currentTilt += tiltInput * tiltSpeed * Time.deltaTime;
+        currentTilt = Mathf.Clamp(currentTilt, -45f, 45f);
+        transform.rotation = Quaternion.Euler(0f, 0f, currentTilt);
     }
 }
